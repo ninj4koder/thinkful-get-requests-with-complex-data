e48ef67b2eff327033f29806e0b70c2a5c50dc89
Marcin Kimak <robbocik@gmail.com>
2020-08-07 19:03:33 -0500

    Handling multiple states, and added addresses field.

--- index.js
+++ index.js
@@ -9,25 +9,30 @@ function formatQueryParams(params) {
 }
 
 function displayResults(responseJson) {
-  // if there are previous results, remove them
   console.log(responseJson);
-  console.log(responseJson.data.length);
+  // if there are previous results, remove them
   $('#results-list').empty();
   $('#results-list').append(
       `<h2>You have ${responseJson.data.length} results</h2>`);
-  // iterate through the items array
-  for (let i = 0; i < responseJson.data.length; i++){
-    // for each video object in the items 
+  // iterate through the data array
+  for (let i = 0; i < responseJson.data.length; i++)  {
+    // for each park in the data 
     //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
+    //list with the full name, description,
+    //and link
     $('#results-list').append(
       `<li><h3>${responseJson.data[i].fullName}</h3>
       <p>${responseJson.data[i].description}</p>
-      <p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
+      <p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>`);
+      for (let j = 0; j < responseJson.data[i].addresses.length; j++)  {
+        $('#results-list').append(
+          `<h4>Address ${j+1}</h4>
+          <p><b>City:</b> ${responseJson.data[i].addresses[j].city}</p>
+          <p><b>Street:</b> ${responseJson.data[i].addresses[j].line1}</p>
+          <p><b>Postal code:</b> ${responseJson.data[i].addresses[j].postalCode}</p>
+          <p><b>State:</b> ${responseJson.data[i].addresses[j].stateCode}</p>`);
+      }
+    };
 };
 
 function getParksList(query, maxResults=10) {
@@ -58,8 +63,10 @@ function getParksList(query, maxResults=10) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const stateCodes = $('#js-state-code').val();
+    let stateCodes = $('#js-state-code').val()
+                                        .replace(/\ /g,'');
     const maxResults = $('#js-max-results').val();
+    $('#results').removeClass('hidden');  //display the results section
     getParksList(stateCodes, maxResults);
   });
 }
